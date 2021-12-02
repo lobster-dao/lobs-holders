3df920b1a00c8f29dd23f1e946532fa8483f0234
Zhongfu Li <me@zhongfu.li>
2021-12-03 07:16:11 +0800

    let's hope this works

--- script.py
+++ script.py
@@ -1,44 +1,76 @@
-# #!/usr/bin/env python3.8
+#!/usr/bin/env python3.8
+import brownie
+import csv
+import os
+import pytz
+from brownie import Contract, chain
+from datetime import datetime
+from pathlib import Path
 
-# import csv
-# import brownie
+github_repo_raw_path = f'https://github.com/{os.environ["GITHUB_REPOSITORY"]}/raw/'
 
-# from brownie import Contract
+def main():
+    brownie.network.connect('mainnet')
 
+    # lobsterdao, or plop your contract addr here
+    nft_contract = Contract('0x026224a2940bfe258d0dbe947919b62fe321f042')
+    block_id = chain.height
+    block_timestamp = datetime.fromtimestamp(chain[block_id].timestamp, tz=pytz.utc)
+    total_supply = nft_contract.totalSupply(block_identifier=block_id)
 
-# def main():
-#     brownie.network.connect('mainnet')
+    # hopefully faster with multicall
+    with brownie.multicall:
+        owners = [nft_contract.ownerOf(i, block_identifier=block_id) for i in range(0, total_supply)]
 
-#     # lobsterdao, or plop your contract addr here
-#     nft_contract = Contract('0x026224a2940bfe258d0dbe947919b62fe321f042')
-#     total_supply = nft_contract.totalSupply()
+    # count per owner address
+    owners = dict()
+    for addr in owners:
+        owners[addr] = owners.get(addr, 0) + 1
 
-#     # hopefully faster with multicall
-#     with brownie.multicall:
-#         owners = [nft_contract.ownerOf(i) for i in range(0, total_supply)]
+    # sort by address
+    owners = dict(sorted(owners.items(), key=lambda x: x[0]))
 
-#     # count num occurrences of each address, then sort in reverse order
-#     owner_addresses = set(owners)
-#     count_per_address = {addr: owners.count(addr) for addr in owner_addresses}
-#     count_per_address = dict(sorted(count_per_address.items(), key=lambda x: x[1], reverse=True))
+    snapshots = Path('snapshots')
+    lobs_owners = snapshots/"lobs_owners"
+    lobs_owners.mkdir(exist_ok=True)
+    lobs_count_by_addr = snapshots/"lobs_count_by_addr"
+    lobs_count_by_addr.mkdir(exist_ok=True)
 
-#     with open('public/index.html', 'w') as f:
-#         writer = csv.writer(f)
-#         writer.writerow(['address', 'count'])
-#         writer.writerows(count_per_address.items())
+    fn_suffix = f'{block_timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")}_blk{block_id}')
 
-#     print('\n'.join([f"{addr}: {count}" for addr, count in count_per_address.items()]))
+    with open(lobs_owners/f"lobs-owners_{fn_suffix}.txt", 'w') as f:
+        f.write('\n'.join(owners.keys()) + '\n')
 
-# if __name__ == '__main__':
-#     main()
+    with open(lobs_count_by_addr/f"lobs-count-by-addr_{fn_suffix}.csv", 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['address', 'count'])
+        writer.writerows(owners.items())
+
+    files = [f.stem for f in lobs_owners.glob('lobs-owners_*_blk*.txt')] + [f.stem for f in lobs_count_by_addr.glob('lobs-count-by-addr_*_blk*.csv')]
+    blkids_dates = sorted(
+                set(
+                    (int(part[1].partition('blk')[-1]), part[0])
+                    for fn in files for part in fn.split('_')),
+                key=lambda x: x[0],
+                reverse=True)
+
+    # honestly, whatever, i'll figure something out later
+    with open('main/public/index.html', 'w') as f:
+        f.write('<html><body><h1>lobs holders snapshots</h1><ul>')
+
+        for blkid, date in blkids_dates:
+            owners_files = list(lobs_owners.glob(f'*_blk{blkid}.txt'))
+            count_by_addr_files = list(lobs_count_by_addr.glob(f'*_blk{blkid}.csv'))
+            owners_link = f'<a href="{github_repo_raw_path + owners_files[0]}">lobs owners</a>' if len(owners_files) == 1 else "lobs owners"
+            count_by_addr_link = f'<a href="{github_repo_raw_path + count_by_addr_files[0]}">lobs count by addr</a>' if len(count_by_addr_files) == 1 else "lobs count by addr"
+            f.write(f'<li>{date}, block {blkid}: {owners_link}, {count_by_addr_link}</li>')
+
+        f.write('</ul></body></html>')
 
 
 if __name__ == '__main__':
-    print("Build start..")
-    
-    import time
-    
-    with open('public/index.html', 'w') as f:
-        f.write("Hello, world! Current UNIX timestamp: " + str(time.time()))
-        
-    print("Success!")
+    print("Build start...")
+
+    main()
+
+    print("Done?")
